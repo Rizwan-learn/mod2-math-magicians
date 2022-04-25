271451a208bb3c8aef3a29573fdd051b94366093
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-04-25 13:34:27 +0100

    import all changes to APP and view page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Calculator from './Component/Calculator';
+import Header from './Component/Header';
+import Home from './Component/Home';
+import Quote from './Component/Quote';
 
 function App() {
   return (
     <div className="App">
-      <Calculator />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
