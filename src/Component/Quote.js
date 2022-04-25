4eff87fd6202d3c883ff19e7efa85472ce048445
Rizwan-learn <92742850+Rizwan-learn@users.noreply.github.com>
2022-04-25 13:34:11 +0100

    Create Quotes JS and CSS

---
+++ src/Component/Quote.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Quote.css';
+
+function Quote() {
+  return (
+    <div className="quote-body">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      </p>
+
+      <br />
+      <br />
+      <span className="writer"> --Rizwan Hasan </span>
+
+    </div>
+  );
+}
+
+export default Quote;
